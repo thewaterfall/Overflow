3534af96ab38f8991b6ffb44cfd723ea3044ffec
THEWaterfall <floodlight98@gmail.com>
2019-04-14 23:02:44 +0300

    Refactored command

--- src/main/java/waterfall/protocol/Command.java
+++ src/main/java/waterfall/protocol/Command.java
@@ -1,9 +1,40 @@
 package waterfall.protocol;
 
-public interface Command {
-    public Command convertToCommand(String toConvert);
+public class Command {
+    private String type;
+    private String status;
+    private String source;
+    private String message;
 
-    public String covertToString(Command command);
+    public String getType() {
+        return type;
+    }
 
-    public Command constructCommand(String toConstruct);
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
---
+++ src/main/java/waterfall/protocol/CommandUtil.java
@@ -0,0 +1,9 @@
+package waterfall.protocol;
+
+public interface CommandUtil {
+    public Command convertToCommand(String toConvert);
+
+    public String covertToString(Command command);
+
+    public Command constructCommand(String toConstruct, String commandType, String from);
+}
