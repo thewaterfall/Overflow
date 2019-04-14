b60a84a713fcacf8f4906ea27e150fcc3991e748
THEWaterfall <floodlight98@gmail.com>
2019-04-14 21:19:52 +0300

    Added server-side interfaces

--- src/main/java/waterfall/communication/server/ClientHandler.java
+++ src/main/java/waterfall/communication/server/ClientHandler.java
@@ -1,4 +1,13 @@
 package waterfall.communication.server;
 
+import waterfall.protocol.Command;
+
 public interface ClientHandler {
+    public void stopConnection();
+
+    public Command receiveRequest(String request);
+
+    public void sendResponse(String response);
+
+    public String processCommand(Command command);
 }
--- src/main/java/waterfall/communication/server/Server.java
+++ src/main/java/waterfall/communication/server/Server.java
@@ -1,4 +1,7 @@
 package waterfall.communication.server;
 
 public interface Server {
+    public void start(int port, int clients);
+
+    public void stop();
 }
