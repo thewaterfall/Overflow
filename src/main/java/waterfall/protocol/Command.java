88e2ee54a81d1fdde1dc628b3386e2e449c57401
THEWaterfall <floodlight98@gmail.com>
2019-04-14 22:48:43 +0300

    Added methods to command inteface

--- src/main/java/waterfall/protocol/Command.java
+++ src/main/java/waterfall/protocol/Command.java
@@ -1,4 +1,9 @@
 package waterfall.protocol;
 
 public interface Command {
+    public Command convertToCommand(String toConvert);
+
+    public String covertToString(Command command);
+
+    public Command constructCommand(String toConstruct);
 }
