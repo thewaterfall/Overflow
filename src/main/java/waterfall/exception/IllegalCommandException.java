0bbb53735a134e049ef566f98ff34ec8ee66b42e
THEWaterfall <floodlight98@gmail.com>
2019-04-15 15:48:37 +0300

    Added IllegalCommandException exception

---
+++ src/main/java/waterfall/exception/IllegalCommandException.java
@@ -0,0 +1,12 @@
+package waterfall.exception;
+
+public class IllegalCommandException extends Exception {
+
+    public IllegalCommandException() {
+
+    }
+
+    public IllegalCommandException(String msg ){
+        super(msg);
+    }
+}
