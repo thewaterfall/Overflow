f8e222cbf4528811bf26e440a0fcead407325115
THEWaterfall <floodlight98@gmail.com>
2019-04-15 15:48:15 +0300

    Refactored CommandUtil interafce

--- src/main/java/waterfall/protocol/CommandUtil.java
+++ src/main/java/waterfall/protocol/CommandUtil.java
@@ -1,9 +1,11 @@
 package waterfall.protocol;
 
+import waterfall.exception.IllegalCommandException;
+
 public interface CommandUtil {
     public Command convertToCommand(String toConvert);
 
     public String covertToString(Command command);
 
-    public Command constructCommand(String toConstruct, String commandType, String from);
+    public Command constructCommand(String toConstruct, String commandType, String from, String status) throws IllegalCommandException;
 }
