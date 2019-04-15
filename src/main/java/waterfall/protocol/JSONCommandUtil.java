c5d6a97fbf692f44c5bfdd4fc7d5621bf32507da
THEWaterfall <floodlight98@gmail.com>
2019-04-15 16:14:43 +0300

    Added JSON implementation of CommandUtil interface

---
+++ src/main/java/waterfall/protocol/JSONCommandUtil.java
@@ -0,0 +1,53 @@
+package waterfall.protocol;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import waterfall.exception.IllegalCommandException;
+
+import java.io.IOException;
+
+public class JSONCommandUtil implements CommandUtil {
+    private ObjectMapper objectMapper;
+
+    public JSONCommandUtil() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public Command convertToCommand(String toConvert) {
+        Command command = null;
+        try {
+            command = objectMapper.readValue(toConvert, Command.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return command;
+    }
+
+    @Override
+    public String covertToString(Command command) {
+        String stringCommand = null;
+        try {
+            stringCommand = objectMapper.writeValueAsString(command);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return stringCommand;
+    }
+
+    @Override
+    public Command constructCommand(String toConstruct, String commandType, String from, String status) throws IllegalCommandException {
+        if(!toConstruct.startsWith("/"))
+            throw new IllegalCommandException("Command should start with '/' symbol");
+
+        String typeCommand = toConstruct.substring(0, toConstruct.indexOf(' '));
+
+        Command command = new Command(commandType, status, from, toConstruct);
+        command.setFullCommand(toConstruct);
+        command.setTypeCommand(typeCommand);
+
+        return command;
+    }
+}
