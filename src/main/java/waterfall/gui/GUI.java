6aca5604353e8de55eaee63dcf1fee1cf0523718
THEWaterfall <floodlight98@gmail.com>
2019-04-17 23:40:32 +0300

    Added a method to GUI inteface

--- src/main/java/waterfall/gui/GUI.java
+++ src/main/java/waterfall/gui/GUI.java
@@ -1,9 +1,13 @@
 package waterfall.gui;
 
+import waterfall.game.Board;
+
 public interface GUI {
     public void write(String message);
 
     public String read();
 
     public void update();
+
+    public void updateBoard(Board board);
 }
