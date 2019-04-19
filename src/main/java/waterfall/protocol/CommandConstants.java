2b74fdec6b4515990c50f30c2ac851b239b4a124
THEWaterfall <floodlight98@gmail.com>
2019-04-19 08:07:30 +0300

    Added command constants

---
+++ src/main/java/waterfall/protocol/CommandConstants.java
@@ -0,0 +1,12 @@
+package waterfall.protocol;
+
+public interface CommandConstants {
+    public static final String COMMAND_STATUS_SUCCESS = "success";
+    public static final String COMMAND_STATUS_FAILURE = "failure";
+
+    public static final String COMMAND_TYPE_REQUEST = "request";
+    public static final String COMMAND_TYPE_RESPONSE = "response";
+
+    public static final String COMMAND_SOURCE_CLIENT = "client";
+    public static final String COMMAND_TYPE_HANDLER = "handler";
+}
