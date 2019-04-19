7a56c1e96de95c917433dc81d8ac990e3eb3b8a9
THEWaterfall <floodlight98@gmail.com>
2019-04-19 08:58:15 +0300

    Added ability to get players from game

--- src/main/java/waterfall/game/Game.java
+++ src/main/java/waterfall/game/Game.java
@@ -1,11 +1,15 @@
 package waterfall.game;
 
+import java.util.List;
+
 public interface Game {
     public String playMove();
 
     public Board getBoard();
 
     public Player getWinner();
+
+    public List<Player> getPlayers();
 
     public boolean isFinished();
 }
