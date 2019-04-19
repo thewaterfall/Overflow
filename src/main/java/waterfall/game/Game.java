e582fc01d9f0b3caad578a6f9040df9816b7671d
THEWaterfall <floodlight98@gmail.com>
2019-04-19 08:40:48 +0300

    Added methods to the game interface

--- src/main/java/waterfall/game/Game.java
+++ src/main/java/waterfall/game/Game.java
@@ -1,4 +1,11 @@
 package waterfall.game;
 
 public interface Game {
+    public String playMove();
+
+    public Board getBoard();
+
+    public Player getWinner();
+
+    public boolean isFinished();
 }
