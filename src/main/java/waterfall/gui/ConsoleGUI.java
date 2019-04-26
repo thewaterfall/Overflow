ae70f8ad97d9fd01c04e098a7794089183f3a7a4
THEWaterfall <floodlight98@gmail.com>
2019-04-26 22:24:10 +0300

    Made ConsoleGUI display colorful board

--- src/main/java/waterfall/gui/ConsoleGUI.java
+++ src/main/java/waterfall/gui/ConsoleGUI.java
@@ -5,14 +5,20 @@ import com.google.inject.Injector;
 import waterfall.communication.client.Client;
 import waterfall.exception.ClientIsStoppedException;
 import waterfall.game.Board;
+import waterfall.game.Color;
+import waterfall.game.Tile;
 import waterfall.injection.Module;
 
 import java.util.Scanner;
+
+import static waterfall.game.ColorCodes.*;
 
 public class ConsoleGUI implements GUI {
     private Client client;
     private Board board;
     private Injector injector;
+
+    char alphaCoordinates[] = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
     private Scanner scanner;
 
@@ -37,7 +43,35 @@ public class ConsoleGUI implements GUI {
 
     @Override
     public void update() {
-        write(board.toString());
+        Tile[][] boardArray = board.getBoardArray();
+        StringBuilder toReturn = new StringBuilder();
+        toReturn.append("    0    1    2    3    4    5    6    7\n\n");
+
+        for (int i = 0; i < boardArray.length; i++) {
+            toReturn.append(alphaCoordinates[i] + "  ");
+            for (int j = 0; j < boardArray.length; j++) {
+                if (boardArray[i][j].getColor().name().equals("White")) {
+                    toReturn.append(WHITE_BACKGROUND_BRIGHT);
+                } else {
+                    toReturn.append(BLACK_BACKGROUND_BRIGHT);
+                }
+
+                if (!boardArray[i][j].getValue().equals("[ ]")) {
+                    if (boardArray[i][j].getPiece().getColor().equals(Color.White)) {
+                        toReturn.append(WHITE_UNDERLINED + boardArray[i][j].getValue());
+                    } else {
+                        toReturn.append(BLACK_UNDERLINED + boardArray[i][j].getValue());
+                    }
+                } else {
+                    toReturn.append(boardArray[i][j].getValue());
+                }
+
+                toReturn.append(RESET + "  ");
+            }
+            toReturn.append("\n\n");
+        }
+
+        write(toReturn.toString());
     }
 
     @Override
